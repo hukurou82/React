ae0fad699874858c189bd607a39a79c1a6f8f462
hukurou82 <49188786+hukurou82@users.noreply.github.com>
2020-04-21 18:31:42 +0900

    todolist

---
+++ src/components/App.js
@@ -0,0 +1,112 @@
+import React from 'react'
+
+import Form from './Form'
+import Todo from './Todo'
+import CheckAll from './CheckAll'
+
+
+let currentId = 0;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos: []
+            /*{
+                id: 0,
+                text: "洗濯する",
+            },
+            {
+                id: 1,
+                text: "宿題する",
+            },
+            {
+                id: 2,
+                text: "腹筋する",
+            }
+        */
+        }
+    }
+    render() {
+        const { todos } = this.state
+        return (
+            <div className="container text-center mt-5" >
+                <h1>todoリスト</h1>
+                <Form onSubmit={this.handleSubmit} />
+
+                <CheckAll allCompleted={
+                    todos.length > 0 && todos.every(({ completed }) => completed)
+                }
+                onChange={this.handleChangeAllCompleted}
+                />
+
+                <select>
+                    <option>全て</option>
+                    <option>未完了</option>
+                    <option>完了済み</option>
+                </select>
+
+                <ul className="list-group">
+                    {todos.map(({ id, text, completed }) => (
+                        <li className="list-group-item" key={id}>
+                            <Todo
+                                id={id}
+                                text={text}
+                                completed={completed}
+                                onChange={this.handleChangeCompleted} />
+                        </li>
+                    ))}
+                </ul>
+
+                <button onClick={this.handleClickDeleteCompleted}>完了済みを全て削除する</button>
+            </div>
+        )
+    }
+
+    handleSubmit = text => {
+        const newTodo = {
+            id: currentId,
+            text,
+            completed: false
+        }
+        const newTodos = [...this.state.todos, newTodo]
+        this.setState({ todos: newTodos })
+        currentId++;
+    }
+
+    handleChangeAllCompleted = completed => {
+        const newTodos = this.state.todos.map(todo => {
+            return{
+                ...todo,
+                completed
+            }
+        })
+        this.setState({todos: newTodos })
+    }
+
+    handleChangeCompleted = (id, completed) => {
+        const newTodos = this.state.todos.map(todo => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    completed,
+                }
+            }
+
+            return todo;
+        })
+
+        this.setState({ todos: newTodos })
+    }
+
+    handleClickDeleteCompleted = () => {
+        const newTodos = this.state.todos.filter(({ completed }) => !completed)
+        this.setState({ todos: newTodos })
+    }
+
+}
+
+
+
+export default App;
